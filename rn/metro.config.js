fb1c4c276b86d0a7b11036901c04bc12a3e3e9ff
Joseph Kim <28643797+jsphkm@users.noreply.github.com>
2026-08-10 23:03:11 -0400

    chore: configure Metro to resolve identity-sdk

---
+++ rn/metro.config.js
@@ -0,0 +1,15 @@
+const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
+
+const projectRoot = __dirname;
+const identitySdk = path.resolve(projectRoot, "../packages/identity-sdk");
+
+const config = getDefaultConfig(projectRoot);
+config.watchFolders = [identitySdk];
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(projectRoot, "../node_modules"),
+];
+config.resolver.disableHierarchicalLookup = true;
+
+module.exports = config;
